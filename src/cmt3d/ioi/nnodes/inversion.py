02d278c04e12b1246cc6cde11c7d7c9eee37b718
Lucas Sawade <lsawade@princeton.edu>
2023-10-04 13:34:56 -0400

    Changed the entire workflow to only use shell script

--- src/cmt3d/ioi/nnodes/inversion.py
+++ src/cmt3d/ioi/nnodes/inversion.py
@@ -108,7 +108,7 @@ def invert_chunk(node: Node):
 
 def cmtinversion(node: Node):
     # node.write(20 * "=", mode='a')
-    concurrent = False
+    node.concurrent = False
 
     node.add(preprocess)
 
@@ -123,7 +123,7 @@ def maybe_invert(node: Node):
         pass
     else:
         # Weighting
-        node.add_mpi(ioi.compute_weights, args=(node.outdir,))
+        node.add(f"cmt3d-ioi weights {node.outdir}", name="Weights")
 
         # Cost, Grad, Hess
         node.add(compute_cgh)
@@ -149,9 +149,8 @@ def preprocess(node: Node):
     if firstiterflag or node.redo or 'FAIL' in status:
 
         # Create the inversion directory/makesure all things are in place
-        node.add(ioi.create_forward_dirs,
-                 args=(node.eventfile, node.inputfile),
-                 name=f"create-dir", cwd=node.log)
+        command = f"cmt3d-ioi create {node.eventfile} {node.inputfile}"
+        node.add(command, name=f"Create-Inv-dir", cwd=node.log)
 
         # Load Green function
         node.add(get_subset)
@@ -169,19 +168,21 @@ def preprocess(node: Node):
         node.add(window, name='window')
 
         # Check Window count
-        node.add_mpi(ioi.check_window_count, args=(node.outdir,))
+        node.add(f"cmt3d-ioi window count {node.outdir}", cwd=node.log,
+                 name='Count-Windows')
 
     # if 'FINISHED' in status:
         # Performs iteration
 
 
 def iteration(node: Node):
+    node.concurrent = False
 
     # Get descent direction
     node.add(compute_descent)
 
     # Computes optimization parameters (Wolfe etc.)
-    node.add(ioi.linesearch, args=(node.outdir, ))
+    node.add(f"cmt3d-ioi linesearch {node.outdir}")
 
     # Runs actual linesearch
     node.add(linesearch)
@@ -196,12 +197,12 @@ def linesearch(node):
 
 
 def search_step(node):
-    node.add(ioi.update_step, args=(node.outdir,))
-    node.add(ioi.update_model, args=(node.outdir,))
+    node.add(f"cmt3d-ioi update-step {node.outdir}", name='Update-Step')
+    node.add(f"cmt3d-ioi model update {node.outdir}", name='Update-Model')
     node.add(forward_frechet_mpi)
     node.add(process_synt_and_dsdm)
     node.add(compute_cgh)
-    node.add(ioi.linesearch, args=(node.outdir,))
+    node.add(f"cmt3d-ioi linesearch {node.outdir}")
     node.add(search_check)
 
 
@@ -228,9 +229,9 @@ def forward_frechet_mpi(node: Node):
     counter += 1
 
     # Forward
-    node.add_mpi(ioi.forward_kernel, nprocs=counter,
-                 args=(node.outdir,), name='forward-frechet-mpi',
-                 cwd=node.log)
+    command = f"cmt3d-ioi forward-kernel {node.outdir}"
+    node.add_mpi(command, nprocs=counter,
+                 name='forward-frechet-mpi', cwd=node.log)
 
 
 def forward(node: Node):
@@ -244,66 +245,15 @@ def frechet(node: Node):
 # ------------------
 # Get the subset
 def get_subset(node: Node):
-    inputparams = cmt3d.read_yaml(os.path.join(node.outdir, 'input.yml'))
 
-    backend = node.backend
-
-    if backend == 'mpi' and True:
-        node.add(get_subset_mpi)
+    if node.db_is_local:
+        flag = "--local"
     else:
-        node.add(get_subset_local)
+        flag = ""
+    command = f"cmt3d-ioi subset {flag} {node.outdir} {node.dbname}"
 
-
-def get_subset_mpi(node: Node):
-    node.add_mpi(ioi.create_gfm, nprocs=1, cpus_per_proc=20,
-                 args=(node.outdir, node.dbname, node.db_is_local),
-                 name='Create_GFM', cwd=node.log)
-
-
-def get_subset_local(node: Node):
-
-    subsetfilename = os.path.join(node.outdir, 'meta', "subset.h5")
-
-    if os.path.exists(subsetfilename) is False:
-
-        if node.db_is_local:
-
-            from glob import glob
-
-            # Check for files given database path
-            db_globstr = os.path.join(node.dbname, '*', '*', '*.h5')
-
-            # Get all files
-            db_files = glob(db_globstr)
-
-            # Check if there are any files
-            if len(db_files) == 0:
-                raise ValueError(f'No files found in {node.dbname} directory. '
-                                 'Please check path.')
-
-            else:
-                # Get subset
-                GFM = GFManager(db_files)
-                GFM.load_header_variables()
-                cmt = ioi.get_cmt(node.outdir, 0, 0)
-                GFM.write_subset_directIO(subsetfilename,
-                                          cmt.latitude, cmt.longitude, cmt.depth_in_m/1000.0,
-                                          dist_in_km=50.0, NGLL=5,
-                                          fortran=False)
-
-        else:
-
-            from gf3d.client import GF3DClient
-            gfc = GF3DClient(db=node.dbname)
-            cmt = ioi.get_cmt(node.outdir, 0, 0)
-            gfc.get_subset(subsetfilename, cmt.latitude, cmt.longitude,
-                           cmt.depth_in_m/1000.0, dist_in_km=50.0, NGLL=5,
-                           fortran=False)
-
-    print('loading gfm')
-    GFM_CACHE[node.eventname] = GFManager(subsetfilename)
-    GFM_CACHE[node.eventname].load()
-    print('loaded  gfm')
+    node.add_mpi(command, nprocs=1, cpus_per_proc=20, name='Getting-Subset',
+                 cwd=node.log)
 
 # ----------
 # Processing
@@ -332,20 +282,17 @@ def process_data(node: Node):
     # Get parameters
     processdict = cmt3d.read_yaml(os.path.join(node.outdir, 'process.yml'))
 
-    for wavetype in processdict.keys():
+    for wave in processdict.keys():
 
         if backend == 'multiprocessing':
-            node.add_mpi(
-                ioi.process_data_wave, args=(node.outdir, wavetype, nproc),
-                nprocs=1, cpus_per_proc=nproc,
-                name=f'process_data_{wavetype}', cwd=node.log)
+            command = f"cmt3d-ioi process data --nproc={nproc} {node.outdir} {wave}"
+            node.add_mpi(command, nprocs=1, cpus_per_proc=nproc,
+                         name=f'process_data_{wave}', cwd=node.log)
 
         elif backend == 'mpi':
-
-            node.add_mpi(
-                ioi.process_data_wave_mpi, args=(node.outdir, wavetype, True),
-                nprocs=nproc,
-                name=f'process_data_{wavetype}_mpi', cwd=node.log)
+            command = f"cmt3d-ioi process data {node.outdir} {wave}"
+            node.add_mpi(command, nprocs=nproc,
+                         name=f'process_data_{wave}_mpi', cwd=node.log)
 
         else:
             raise ValueError('Double check your backend/multiprocessing setup')
@@ -362,21 +309,18 @@ def process_synthetics(node: Node):
     # Get parameters
     processdict = cmt3d.read_yaml(os.path.join(node.outdir, 'process.yml'))
 
-    for wavetype in processdict.keys():
+    for wave in processdict.keys():
 
         if nproc == 1 or backend == 'multiprocessing':
             # Process the normal synthetics
-            node.add_mpi(
-                ioi.process_synt_wave, args=(node.outdir, wavetype, nproc),
-                nprocs=1, cpus_per_proc=nproc,
-                name=f'process_synt_{wavetype}',
-                cwd=node.log)
+            command = f"cmt3d-ioi process synt --nproc={nproc} {node.outdir} {wave}"
+            node.add_mpi(command, nprocs=1, cpus_per_proc=nproc,
+                         name=f'process_synt_{wave}', cwd=node.log)
 
         elif nproc > 1 and backend == 'mpi':
-            node.add_mpi(
-                ioi.process_synt_wave_mpi, args=(node.outdir, wavetype),
-                nprocs=nproc, name=f'process_synt_{wavetype}_mpi',
-                cwd=node.log)
+            command = f"cmt3d-ioi process synt {node.outdir} {wave}"
+            node.add_mpi(command, nprocs=nproc,
+                         name=f'process_synt_{wave}_mpi', cwd=node.log)
         else:
             raise ValueError('Double check your backend/multiprocessing setup')
 
@@ -397,25 +341,21 @@ def process_dsdm(node: Node):
     NM = len(ioi.read_model_names(node.outdir))
 
     # Loop over wave types and model parameters
-    for wavetype in processdict.keys():
+    for wave in processdict.keys():
 
         for _i in range(NM):
 
             if nproc == 1 or backend == 'multiprocessing':
-                node.add_mpi(
-                    ioi.process_dsdm_wave, args=(
-                        node.outdir, _i, wavetype, True),
-                    nprocs=1, cpus_per_proc=nproc,
-                    name=f'process_dsdm{_i:05d}_{wavetype}',
-                    cwd=node.log)
+                command = f"cmt3d-ioi process dsdm --nproc={nproc} {node.outdir} {_i} {wave}"
+                node.add_mpi(command, nprocs=1, cpus_per_proc=nproc,
+                             name=f'process_dsdm{_i:05d}_{wave}',
+                             cwd=node.log)
 
             elif nproc > 1 and backend == 'mpi':
-                node.add_mpi(
-                    ioi.process_dsdm_wave_mpi, args=(
-                        node.outdir, _i, wavetype, True),
-                    nprocs=1, cpus_per_proc=nproc,
-                    name=f'process_dsdm{_i:05d}_{wavetype}_mpi',
-                    cwd=node.log)
+                command = f"cmt3d-ioi process dsdm {node.outdir} {_i} {wave}"
+                node.add_mpi(command, nprocs=1, cpus_per_proc=nproc,
+                             name=f'process_dsdm{_i:05d}_{wave}_mpi',
+                             cwd=node.log)
 
             else:
                 raise ValueError(
@@ -434,54 +374,42 @@ def window(node: Node):
     # Get parameters
     processdict = cmt3d.read_yaml(os.path.join(node.outdir, 'process.yml'))
 
-    for wavetype in processdict.keys():
+    for wave in processdict.keys():
 
         if backend == 'multiprocessing':
-            # Process the normal synthetics
-            node.add_mpi(ioi.window_wave, args=(node.outdir, wave, nproc),
-                         nprocs=1, cpus_per_proc=nproc,
-                         name=f'window_{wavetype}',
+            # Window using multiprocessing
+            command = f"cmt3d-ioi window select --nproc={nproc} {node.outdir} {wave}"
+            node.add_mpi(command, nprocs=1, cpus_per_proc=nproc,
+                         name=f'window_{wave}',
                          cwd=node.log)
 
         elif backend == 'mpi':
-
-            node.add_mpi(
-                ioi.window_wave_mpi, args=(node.outdir, wavetype, True),
-                nprocs=nproc, name=f'window_{wavetype}_mpi',
-                cwd=node.log)
+            command = f"cmt3d-ioi window select {node.outdir} {wave}"
+            node.add_mpi(command, nprocs=nproc,
+                         name=f'window_{wave}_mpi', cwd=node.log)
         else:
-
             raise ValueError('Double check your backend/multiprocessing setup')
 
 
 # ------------------
 # Updating the model
 
-# update linesearch
-
-
-def compute_new_model(node: Node):
-    ioi.update_model(node.outdir)
-
-
 # Transer to next iteration
 def transfer_mcgh(node: Node):
-    node.add(ioi.update_mcgh, args=(node.outdir,),
-             name=f"transfer-mcgh", cwd=node.log)
+    command = f"cmt3d-ioi model transfer {node.outdir}"
+    node.add(command, name='Model-Transfer')
 
 
 # -------------
 # Pre-inversion
 def compute_weights(node: Node):
-    node.add_mpi(
-        ioi.compute_weights,  args=(node.outdir,),
-        nprocs=1, cpus_per_proc=4,
-        name=f"compute-weights",
-        cwd=node.log)
-
+    command = f"cmt3d-ioi weights {node.outdir}"
+    node.add(command, name='Model-Update')
 
 # --------------------------------
 # Cost, Gradient, Hessian, Descent
+
+
 def compute_cgh(node: Node):
     node.concurrent = True
     node.add(compute_cost)
@@ -492,30 +420,29 @@ def compute_cgh(node: Node):
 
 
 def compute_cost(node: Node):
-    node.add_mpi(ioi.cost, args=(node.outdir,), name=f"cost", cwd=node.log)
+    command = f"cmt3d-ioi cost {node.outdir}"
+    node.add(command, name=f"cost", cwd=node.log)
 
 
 # Gradient
 def compute_gradient(node: Node):
-    node.add_mpi(ioi.gradient, args=(node.outdir,), name=f"grad", cwd=node.log)
+    command = f"cmt3d-ioi gradient {node.outdir}"
+    node.add(command, name=f"grad", cwd=node.log)
 
 
 # Hessian
 def compute_hessian(node: Node):
-    node.add_mpi(ioi.hessian, args=(node.outdir,), name=f"hess", cwd=node.log)
+    command = f"cmt3d-ioi hessian {node.outdir}"
+    node.add(command, name=f"hess", cwd=node.log)
 
 
 # Descent
 def compute_descent(node: Node):
-    node.add_mpi(ioi.descent, args=(node.outdir,),
-                 name=f"descent", cwd=node.log)
+    command = f"cmt3d-ioi descent {node.outdir}"
+    node.add_mpi(command, name=f"descent", cwd=node.log)
 
 # ----------
 # Linesearch
-
-
-def compute_optvals(node: Node):
-    node.add_mpi(ioi.check_optvals, args=(node.outdir,))
 
 
 # Check whether to add another iteration
@@ -528,12 +455,14 @@ def iteration_check(node: Node):
 
     elif flag == "SUCCESS":
         if ioi.check_done(node.outdir) is False:
-            node.add(ioi.update_iter, args=(node.outdir,))
-            node.add(ioi.reset_step, args=(node.outdir,))
+            node.add
+            node.add(
+                f"cmt3d-ioi update-iter {node.outdir}", name="Update-iter")
+            node.add(f"cmt3d-ioi reset-step {node.outdir}", name="Reset-step")
             node.parent.parent.add(iteration)
         else:
-            node.add(ioi.update_iter, args=(node.outdir,))
-            node.add(ioi.reset_step, args=(node.outdir,))
+            node.add(f"cmt3d-ioi update-iter {node.outdir}")
+            node.add(f"cmt3d-ioi reset-step {node.outdir}")
             node.rm(os.path.join(node.outdir, 'meta', 'subset.h5'))
 
 
@@ -551,3 +480,6 @@ def search_check(node: Node):
     elif flag == "ADDSTEP":
         # Update step
         node.parent.parent.add(search_step)
+
+
+# %%
